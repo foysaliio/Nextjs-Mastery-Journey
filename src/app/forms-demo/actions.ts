0eeb0eaea569c387a18b0d2a8a924ab9218ecf30
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-22 02:53:41 +0600

    feat(nextjs): work with form data

--- src/app/forms-demo/actions.ts
+++ src/app/forms-demo/actions.ts
@@ -4,8 +4,9 @@ export const submitProfile = async (FormData: FormData): Promise<void> => {
   const name = FormData.get("name");
   const email = FormData.get("email");
 
-  console.log({
-    name,
-    email,
-  });
+  const allEntries = Object.fromEntries(FormData.entries());
+
+  console.log("Name :", name);
+  console.log("Email :", email);
+  console.log("All Form Data :", allEntries);
 };
